06da218ccf1a87f47545c72242f32ea26405c04f
juanmafrick04 <juanmartinfrick@gmail.com>
2025-06-25 19:51:22 -0300

    Fixed bugs

--- routes/favoritesRoutes.js
+++ routes/favoritesRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const favoritesController = require("../controllers/favouritesController");
+const favoritesController = require("../controllers/favoritesController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/", authMiddleware, favoritesController.addFavorite);
